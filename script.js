efbc623f2c5c211ce68e414d8894768d52aea98c
Glitch (glitch-hello-website) <none>
2022-06-08 22:32:48 +0000

    🎬🎡 Checkpoint ./README.md:42503019/2026 ./index.html:42503019/20524 ./script.js:42503019/1269 ./style.css:42503019/5940

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
